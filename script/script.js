ca80f273bc6311e03430ebc71dae59c54e23168b
Ivan Kornienko <kornienko.ivn@gmail.com>
2021-01-23 17:14:54 +0300

    Добавить попап для карточки с местом

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// попап ред. имя
 let editButton = document.querySelector('.profile-info__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
@@ -33,3 +34,66 @@ function handleFormSubmit (evt) {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
+
+
+
+// карточки
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.element_template').content;
+const addButton = document.querySelector('.profile__add-button');
+
+function render() {
+	initialCards.forEach(renderElement);
+}
+function renderElement(data) {
+	const cloneElement = elementTemplate.cloneNode(true);
+    cloneElement.querySelector('.element__heading').innerText = data.name;
+    cloneElement.querySelector('.element__image').src = data.link;
+    //cloneElement.querySelector('.element__image').setAttribute("alt",{data.name});
+
+    setListeners(cloneElement);
+	elements.prepend(cloneElement);
+}
+render();
+
+function setListeners(element) {
+    element.querySelector('.element__delete').addEventListener('click', handleDelete);
+    element.querySelector('.element__fovorite-button').addEventListener('click', handleLike);
+    } 
+
+function handleDelete(evt) {
+        evt.target.closest('.element').remove();
+        //resetEditMode();
+    }
+function handleLike(evt) {
+  evt.target.closest('.element__fovorite-button').classList.toggle("element__fovorite-button_active");
+} 
+
